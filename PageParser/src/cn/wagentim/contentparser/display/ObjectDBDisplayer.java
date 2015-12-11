22e0723598fc58b526582e724076855d2f533dd5
wagentim <bhwerbung@gmail.com>
2015-12-11 13:39:33 +0100

    remove unused import

--- PageParser/src/cn/wagentim/contentparser/display/ObjectDBDisplayer.java
+++ PageParser/src/cn/wagentim/contentparser/display/ObjectDBDisplayer.java
@@ -3,7 +3,6 @@ package cn.wagentim.contentparser.display;
 import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
